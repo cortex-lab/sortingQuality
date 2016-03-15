e1c6aef8d6b04f1fa8fc58f98ebbdab4f8aa5b6c
nsteinme <nick.steinmetz@gmail.com>
2016-03-15 19:54:30 +0000

    some status messages for kwik

--- maskedClusterQualityKwik.m
+++ maskedClusterQualityKwik.m
@@ -6,6 +6,7 @@ function [clusterIDs, unitQuality, contaminationRate] = maskedClusterQualityKwik
 %   baseFilename  = '/data/nick/M150218_NS1LAV/20150601/20150601_all';
 % end
 
+fprintf(1, 'loading data from kwik and kwx...\n');
 clusterNames =  hdf5read([baseFilename '.kwik'], ['/channel_groups/' num2str(shankIndex) '/spikes/clusters/main']);
 featuresAndMasks = hdf5read([baseFilename '.kwx'],  ['/channel_groups/' num2str(shankIndex) '/features_masks']); %  Size is [2, nChan*3, nSpikes]
 
@@ -14,4 +15,5 @@ features = squeeze(featuresAndMasks(1,:,:))';
 masks = squeeze(featuresAndMasks(2,:,:))';
 clear featuresAndMasks
 
+fprintf(1, 'computing qualities...\n');
 [clusterIDs, unitQuality, contaminationRate] = maskedClusterQualityDense(clusterNames, features, masks);
